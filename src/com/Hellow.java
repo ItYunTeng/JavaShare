4c23c49ed1bcfc611824288ff477a8ac885d56e8
JoySu <781290430@qq.com>
2018-01-25 15:29:12 +0800

    update

--- src/com/Hellow.java
+++ src/com/Hellow.java
@@ -3,5 +3,12 @@ package com;
 public class Hellow {
 	public static void main(String[] args) {
 		System.out.println("nihagit");
+		Hellow hellow = new Hellow();
+		hellow.sss();
 	}
+	
+	public void sss() {
+		System.out.println();
+	}
+	
 }
